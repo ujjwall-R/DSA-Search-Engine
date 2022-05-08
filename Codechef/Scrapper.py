a804ac7ec6576ca8e1d54629625b2b21065710bb
Ujjwal R <ujjwal.raj.5798@gmail.com>
2022-05-09 02:25:30 +0530

    saving problems in different files implemented

--- Codechef/Scrapper.py
+++ Codechef/Scrapper.py
@@ -27,11 +27,36 @@ if len(all_ques) > 0:
     for ques in all_ques:
         urls.append("https://www.codechef.com"+ques['href'])
         titles.append(ques.text)
-
+    print("Storing problems urls....")
     with open(f'{problemTag}/problem_urls.txt', "w+") as f:
         f.write('\n'.join(urls))
+        print("Problems urls successfully saved!")
+    time.sleep(0.4)
+    print("Storing problems titles....")
     with open(f'{problemTag}/problem_titles.txt', "w+") as f:
         f.write('\n'.join(titles))
+        print("Problems titles successfully saved!")
+    time.sleep(0.4)
     print(f'Done! Check the {problemTag} folder to see the data sheet')
+    time.sleep(0.5)
+    print("Initialising to store each problem in /Problem directory...")
+    os.mkdir(f'{problemTag}/Problems')
+    print("Started storing problems....")
+    for title in titles:
+        currQCode = title.split(' ')[-1].strip()
+        print(f'Current problem: {currQCode}')
+        url_ = f'https://www.codechef.com/problems/{currQCode}'
+        driver.get(url_)
+        time.sleep(5)
+        html = driver.page_source
+        soup = BeautifulSoup(html, 'html.parser')
+        problem_statement = soup.find(
+            'div', {'class': 'problem-statement'}).text
+        print("")
+        with open(f'{problemTag}/Problems/{currQCode}.txt', "w+") as f:
+            f.write(problem_statement)
+            print(f'{currQCode} saved successfully!')
+        # print(str(problem_statement))
+    print("All problems saved successfully!")
 else:
     print("No such tags found!")
