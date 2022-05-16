6364491cb756180d9e2f5ddd51114897c575d916
Ujjwal R <ujjwal.raj.5798@gmail.com>
2022-05-17 00:47:09 +0530

    tfIdf done

--- backend/itf_calculator.js
+++ backend/itf_calculator.js
@@ -5,28 +5,11 @@ const { normalize } = require("path");
 var path = require("path");
 var process = require("process");
 
-const ProblemDirectory = "../DataSet/Problems/";
-
-// Loop through all the files in the temp directory and store every problem statement in a all.txt file
-function readFiles(dirname) {
-  fs.readdir(dirname, function (err, filenames) {
-    if (err) {
-      // onError(err);
-      return;
-    }
-    filenames.forEach(function (filename) {
-      fs.readFile(dirname + filename, "utf-8", function (err, content) {
-        if (err) {
-          // onError(err);
-          return;
-        }
-        fs.appendFile("all.txt", content, function (err) {
-          if (err) throw err;
-        });
-      });
-    });
-  });
+function countOccurencesStr(string, word) {
+  return string.split(word).length - 1;
 }
+
+const ProblemDirectory = "../DataSet/Problems/";
 
 const totalNoOfDoc = fs.readdirSync(ProblemDirectory).length;
 
@@ -66,38 +49,78 @@ readFiles2().then((data) => {
   data.forEach((d) => {
     strAll += d.value;
   });
-  const totalKeys = keyword_extractor.extract(strAll.toLowerCase(), {
-    language: "english",
-    remove_digits: true,
-    return_changed_case: true,
-    remove_duplicates: true,
-  });
+  const totalKeys = keyword_extractor
+    .extract(strAll.toLowerCase(), {
+      language: "english",
+      remove_digits: true,
+      return_changed_case: true,
+      remove_duplicates: true,
+    })
+    .sort();
   // console.log(totalKeys);
 
   let itfValuesOfAllKeywords = [];
+  let tfmatrix = [];
+  for (let j = 0; j < totalNoOfDoc; j++) {
+    tfmatrix[j] = [];
+  }
+  for (let j = 0; j < totalNoOfDoc; j++) {
+    for (let i = 0; i < totalKeys.length; i++) {
+      tfmatrix[j][i] = 0;
+    }
+  }
+  // console.log(tfmatrix);
   for (let i = 0; i < totalKeys.length; i++) {
     itfValuesOfAllKeywords[i] = 0;
   }
+
   for (let row = 0; row < totalNoOfDoc; row++) {
-    const curr_keyWordArray = keyword_extractor.extract(
-      data[row].value.toLowerCase(),
-      {
+    const curr_keyWordArray = keyword_extractor
+      .extract(data[row].value.toLowerCase(), {
         language: "english",
         remove_digits: true,
         return_changed_case: true,
         remove_duplicates: true,
-      }
-    );
+      })
+      .sort();
+    const temp = curr_keyWordArray.length;
     let count = 0;
     for (let col = 0; col < totalKeys.length; col++) {
       const keyW = totalKeys[col];
       if (curr_keyWordArray.includes(keyW)) itfValuesOfAllKeywords[col]++;
+      tfmatrix[row][col] = countOccurencesStr(
+        data[row].value.toLowerCase(),
+        keyW.toLowerCase()
+      );
+      tfmatrix[row][col] /= temp;
     }
   }
   for (let i = 0; i < totalKeys.length; i++) {
     itfValuesOfAllKeywords[i] = totalNoOfDoc / itfValuesOfAllKeywords[i];
   }
-  console.log(itfValuesOfAllKeywords); //ITF matrix
+  // console.log(itfValuesOfAllKeywords); //ITF matrix
+  // console.log(tfmatrix); //tf matrix
+
+  let similarityMatrix = [];
+  for (let j = 0; j < totalNoOfDoc; j++) {
+    similarityMatrix[j] = [];
+  }
+  for (let j = 0; j < totalNoOfDoc; j++) {
+    for (let i = 0; i < totalKeys.length; i++) {
+      similarityMatrix[j][i] = tfmatrix[j][i] * itfValuesOfAllKeywords[i];
+    }
+  }
+  console.log(similarityMatrix); //similarity matrix
+
+  const sim = { similarityArray: similarityMatrix };
+  console.log(sim);
+  const data2 = JSON.stringify(sim);
+  fs.writeFile("similarityMatrix.json", data2, (err) => {
+    if (err) {
+      throw err;
+    }
+    console.log("JSON data is saved.");
+  });
 });
 
 //no. of columns=length of keywordArrayOfAll
